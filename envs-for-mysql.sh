54bba4a609e190d829c7ddf8fd563ee284f98870
iamprakkie <gprakk.ie@gmail.com>
2023-04-06 13:47:15 -0600

    initial commit

--- envs-for-mysql.sh
+++ envs-for-mysql.sh
@@ -1,3 +1,6 @@
+export MYSQL_PATH=$(which mysql)
+export MYSQL_DIR=$(dirname $MYSQL_PATH)
+
 export MYREGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/region)
 aws configure set region $MYREGION
 export MYSQL_REGION=$(aws cloudformation describe-stacks --stack-name 'mySQLBenchmarking' --query "Stacks[][].Outputs[?OutputKey=='mysqlRegion'].OutputValue" --output text)
