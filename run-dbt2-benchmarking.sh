01d745399683e976b29998f61ba613d32e25f225
iamprakkie <gprakk.ie@gmail.com>
2023-04-07 11:49:23 -0600

    initial commit

--- run-dbt2-benchmarking.sh
+++ run-dbt2-benchmarking.sh
@@ -15,7 +15,3 @@ source /home/ssm-user/mysql-dbt2-benchmarking/envs-for-mysql.sh
 
 log "Benchmarking.."
 /home/ssm-user/dbt2/dbt2-0.37.50.16/scripts/run_mysql.sh --connections $CONNECTIONS --time $RUN_SECONDS --warehouses $WAREHOUSE_COUNT --zero-delay --host $MYSQL_HOST_IP --user benchmarker --password $BENCHMARKER_PWD
-
---tot-warehouses
---spread
---terminals
