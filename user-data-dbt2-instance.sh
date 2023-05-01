58fb089ae77c70347d2fc77b70c433b5e8d0d716
iamprakkie <gprakk.ie@gmail.com>
2023-04-30 19:44:42 -0600

    WIP

--- user-data-dbt2-instance.sh
+++ user-data-dbt2-instance.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# format volume /dev/sda1 and mount to /dbt2-data
-mkfs -t xfs /dev/sda1
-mkdir /dbt2-data
-mount /dev/sda1 /dbt2-data
-cp /etc/fstab /etc/fstab.orig
-echo -e "UUID=`sudo blkid /dev/sda1 -s UUID -o value`\t/dbt2-data\txfs\tdefaults,nofail  0  2" >> /etc/fstab
+# format volume /dev/sda1 and mount to /dbt2-data if you plan to use separate mount
+# mkfs -t xfs /dev/sda1
+# mkdir /dbt2-data
+# mount /dev/sda1 /dbt2-data
+# cp /etc/fstab /etc/fstab.orig
+# echo -e "UUID=`sudo blkid /dev/sda1 -s UUID -o value`\t/dbt2-data\txfs\tdefaults,nofail  0  2" >> /etc/fstab
 
 # install mysql client 8.0.32
 yum update -y
